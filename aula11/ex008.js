024d23515dac76a1c6c7bb61203d314b63be7353
oliveira00008 <gabrielgithub00008@gmail.com>
2023-11-08 02:22:54 -0300

    atualisando desfio

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+//essa seria uma condiçao simples 
+var vel = 30 //sempre que eu quiser escrever eu uso console.log 
+console.log(`A velocidade do seu carro e ${vel} km/`)
+if (vel > 60) {//e uma condiçao simples 
+    console.log(`voce ultrapaçou a velocidade permitida`)
+}
+console.log(`dirija sempre usando cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'reino unido'
+console.log(`vivendo em ${pais}`)
+if (pais != 'brasil') {
+    console.log('voce e estrangeiro')
+}else {
+    console.log('voce e brasileiro')
+}
