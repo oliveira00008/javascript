c5466f0325acc1fd71986f9226d0e6481809142c
oliveira00008 <gabrielgithub00008@gmail.com>
2023-11-21 01:28:09 -0300

    atuali comit

---
+++ aula13/anbiente.js
@@ -0,0 +1,21 @@
+/*console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')*/
+
+//se eu quiser colocar 10x esse tudo bem sem muitas linhas 
+
+/*var c = 1 
+while(c <= 6){
+    console.log('tudo bem')
+    c++ //e o mesmo que 1 + 1
+}*/
+
+//e temos a extrutura DO que e a condiçao no final
+
+var c = 1 
+do{
+    console.log('te amo carina')
+    c++ //e o mesmo que 1 + 1
+}while(c <= 6)
---
+++ aula14/anbiente.js
@@ -0,0 +1,6 @@
+//nessa linha de repetiçao eu tenho 3 parte inicio , texto e incremetaçao.
+console.log('vai começar')
+for (var c =1 ; c <= 10; c++) {
+    console.log(c)
+}
+console.log('FIM')
