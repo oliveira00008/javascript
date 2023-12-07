57204a5eb4b3a756d53ce308a88ba5d8c27c8744
oliveira00008 <gabrielgithub00008@gmail.com>
2023-12-07 04:14:26 -0300

    atualisei meu exercicio

--- aula14ex/script.js
+++ aula14ex/script.js
@@ -2,10 +2,30 @@ function contar() {
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
+    let res =  document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Falta dados')
+        window.alert('[ERRO] Falta dados!')
     } else {
-        alert('ok')
+       res.innerHTML = 'Contando:'
+       let i = Number(ini.value)
+       let f = Number(fim.value)
+       let p = Number(passo.value)
+       if(p <= 0){
+        window.alert('passo invalido! Consideramos passo 1')  
+        p = 1
+       }
+       if (i < f) {
+        //contagen crescente
+           for(var c = i; c <= f; c += p){
+               res.innerHTML += `${c} \u{1F911}`
+             
+       }         
+    } else {
+        //contagen regreciva
+        for(let c = i; c >= f; c -= p)
+        res.innerHTML += `${c} \u{1F911}`
+    }
+    res.innerHTML += `\u{1F975}`
     }
 }
